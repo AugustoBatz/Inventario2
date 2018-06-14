98b9ee329c314bb02a87b291223736704dbb8bee
SYS0xWord <miguelbatz2@gmail.com>
2018-06-13 23:13:44 -0600

    Dos triggers

--- triggers.sql
+++ triggers.sql
@@ -11,3 +11,16 @@ BEGIN
         
     END;//
 delimiter ;  
+delimiter //
+DROP TRIGGER IF EXISTS RestarExistencias//
+CREATE TRIGGER RestarExistencias
+AFTER INSERT on LoteVenta
+FOR EACH ROW
+BEGIN
+        
+        
+        
+        Update	 Producto set Producto.Existencia=Producto.Existencia-new.Cantidad where Producto.id=new.Producto_id;
+        
+    END;//
+delimiter ;  
